d48b1f08c1fcc44e61d40bd130cc031377a95fbd
poonyanuch <poonyanuch-s@hotmail.com>
2015-04-23 23:11:36 +0700

    First copy of 4 files

---
+++ code1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(int argc,char*argv[])
+{
+	char * name = argv[0];
+	int child_pid = fork();
+
+	if(child_pid == 0)
+	{
+
+		printf("child of %s is %d\n",name,getpid());
+ 		execl("/bin/ls","ls","-a",(char*)NULL);
+		return 0;
+	}
+	else{
+
+		printf("my child is %d\n",child_pid);
+		return(0);
+	    }
+}
+
+//execl("/bin/ls","ls","-a",(char*)NULL);
